f039e7e9b42e9b332f64b5501089bd7c2ee8ca3d
MattRodigheri <mrodigheri@gmail.com>
2018-07-30 12:12:57 -0700

    Completed Implementation for StringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,54 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var stringed = '';
+
+  var type = typeof obj;
+
+  if (type === 'number' || type === 'boolean' || obj === null) {
+    stringed = stringed.concat(String(obj));
+  } else if (type === 'string') {
+    stringed = stringed.concat('"' + obj + '"');
+  } else if (type === 'undefined' || type === 'function') {
+    return '';
+  } else if (Array.isArray(obj)) {
+    stringed = stringed.concat('[');
+    for (var i = 0; i < obj.length; i++) {
+      stringed = stringed.concat(stringifyJSON(obj[i]), ',');
+      
+      /*if (typeof obj[i] === 'object') {
+        stringifyJSON(obj[i]);
+      } else {
+        stringed.concat(processValues(val[i]));
+      }
+*/
+    }
+    if(stringed.length !== 1) {
+      stringed = stringed.slice(0, stringed.length - 1);
+    }
+    stringed = stringed.concat(']');
+  } else /*OBJECT*/ {
+    stringed = stringed.concat('{');
+    for (var key in obj) {
+      if (stringifyJSON(obj[key]) !== '') {
+        stringed = stringed.concat('\"' + String(key) + '\":' + stringifyJSON(obj[key]) + ',');
+      }
+    }
+    if(stringed.length !== 1) {
+      stringed = stringed.slice(0, stringed.length - 1);
+    }
+    stringed = stringed.concat('}');
+  }
+  return stringed;
 };
+
+
+/*
+var processValues = function(val) {
+  var type = typeof val;
+  if (type === 'number') {
+    return String(val);
+  }
+
+}
+*/
